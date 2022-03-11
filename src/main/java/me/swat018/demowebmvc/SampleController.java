9b7243e95b856970080119749042ad8a2d8c5ffa
jinwoo park <swat018@gmail.com>
2022-03-11 13:32:27 +0900

    feat : 핸들러 메서드 3부 - 요청 매개변수 (단순 타입)

--- src/main/java/me/swat018/demowebmvc/SampleController.java
+++ src/main/java/me/swat018/demowebmvc/SampleController.java
@@ -1,27 +1,19 @@
 package me.swat018.demowebmvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 public class SampleController {
 
-//    @GetMapping("/events/{id}")
-//    @ResponseBody
-//    public Event getEvent(@PathVariable Integer id){
-//        Event event = new Event();
-//        event.setId(id);
-//        return event;
-//    }
-    @GetMapping("/events/{id}")
+    @PostMapping("/events")
     @ResponseBody
-    public Event getEvent(@PathVariable("id") Integer idValue, @MatrixVariable String name){
+    public Event getEvent(@RequestParam Map<String, String> params, @RequestParam Integer limit){
         Event event = new Event();
-        event.setId(idValue);
-        event.setName(name);
+        event.setName(params.get("name"));
+        event.setLimit(limit);
         return event;
     }
 }
--- src/test/java/me/swat018/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/swat018/demowebmvc/SampleControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,18 +20,13 @@ public class SampleControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-//    @Test
-//    public void getEvent() throws Exception {
-//        mockMvc.perform(get("/events/1"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("id").value(1));
-//    }
     @Test
     public void getEvent() throws Exception {
-        mockMvc.perform(get("/events/1;name=jinwoo"))
+        mockMvc.perform(post("/events")
+                        .param("name","jinwoo")
+                        .param("limit", "20"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("id").value(1));
+                .andExpect(jsonPath("name").value("jinwoo"));
     }
 }
